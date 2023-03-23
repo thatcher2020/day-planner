e8e1bbf1904b6172788089c714f812b681213a0d
Joseph Thatcher <thatcherjoe20@gmail.com>
2023-03-23 19:52:57 -0400

    added more time slots(time color switch not working)

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -20,54 +20,25 @@ $(document).ready(function (){
   // saving to local storage
     $('.saveBtn').on('click', function(){
       var time = $(this).parent().attr('id')
-      var text = $(this).siblings('.description').val()
+      var text = $(this).siblings('.typed').val()
       localStorage.setItem(time, text)
     })
 
   // getting what was saved from local
-    $('#hour-9 .description').val(localStorage.getItem('hour-9'))
-    $('#hour-10 .description').val(localStorage.getItem('hour-10'))
-    $('#hour-11 .description').val(localStorage.getItem('hour-11'))
-    $('#hour-12 .description').val(localStorage.getItem('hour-12'))
-    $('#hour-13 .description').val(localStorage.getItem('hour-13'))
-    $('#hour-14 .description').val(localStorage.getItem('hour-14'))
-    $('#hour-15 .description').val(localStorage.getItem('hour-15'))
-    $('#hour-16 .description').val(localStorage.getItem('hour-16'))
-    $('#hour-17 .description').val(localStorage.getItem('hour-17'))
-
+    $('#hour-9 .typed').val(localStorage.getItem('hour-9'))
+    $('#hour-10 .typed').val(localStorage.getItem('hour-10'))
+    $('#hour-11 .typed').val(localStorage.getItem('hour-11'))
+    $('#hour-12 .typed').val(localStorage.getItem('hour-12'))
+    $('#hour-13 .typed').val(localStorage.getItem('hour-13'))
+    $('#hour-14 .typed').val(localStorage.getItem('hour-14'))
+    $('#hour-15 .typed').val(localStorage.getItem('hour-15'))
+    $('#hour-16 .typed').val(localStorage.getItem('hour-16'))
+    $('#hour-17 .typed').val(localStorage.getItem('hour-17'))
+    $('#hour-18 .typed').val(localStorage.getItem('hour-18'))
+    $('#hour-19 .typed').val(localStorage.getItem('hour-19'))
+    $('#hour-20 .typed').val(localStorage.getItem('hour-20'))
+    
+    
   // displaying current date in header
     $('#currentDay').text(dayjs().format('dddd, MMMM D'))
   })
-
-
-
-
-
-
-
-
-
-
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-});
